03c2e03c51b9536b5c209751226771b5e4cafdaf
zalimus <zalim011@gmail.com>
2023-01-20 00:45:01 +0300

    add solution for task 8

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -1,9 +1,15 @@
-﻿string? inputNum = Console.ReadLine();
+﻿// Считываем данные с консоли
+string? inputNum = Console.ReadLine();
 
+// Проверяем, чтобы данные были не пустыми
 if(inputNum!=null)
 {
+    // Парсим введеное число
     int inputNumber1 = int.Parse(inputNum);
+
+    // Создаем пустую строку
     string result = string.Empty;
+
     for(int i = 2; i < inputNumber1; i = i+2)
     {
         result = result + i + ",";
