cd78a20dd9574214992477c2786cf010c50ff5c4
zalimus <zalim011@gmail.com>
2023-01-30 16:04:29 +0300

    add solution for task 19 21 23

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,43 @@
+﻿// №19 Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет 
+// 23432 -> да 
+// 12821 -> да
+// * Сделать вариант через СЛОВАРЬ четырехзначных палиндромов
+
+// Метод читает данные от пользователя
+int ReadData(string msg) 
+
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Выводим результат пользователю
+void PrintQuterAnswer(bool palin)
+{
+    if (palin == true) Console.WriteLine("Число является палиндромом");
+    if (palin == false) Console.WriteLine("Число не является палиндромом");
+}
+
+// Вычисляем результат
+bool PalinTest(int n)
+{
+    bool res = false;
+    int digit1 = n/10000;
+    int digit2 = (n/1000)%10;
+    int digit3 = (n/10)%10;
+    int digit4 = n%10;
+
+    res = (digit1 == digit4) && (digit2 == digit3)?true:false;
+
+    return res;
+}
+
+int num = ReadData("Введите пятизначное число: ");
+bool value = PalinTest(num);
+
+PrintQuterAnswer(value);
+
+
+
---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+// * Вывести таблицу с границами и значениями друг над другом
+
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Выводим результат пользователю
+void PrintData(string msg1, string msg2)
+{
+    Console.WriteLine(msg1);
+    Console.WriteLine(msg2);
+}
+
+// Вывод нахождения квадрата чисел от 1 до n
+string LineBilder(int n, int p)
+{
+    string s = string.Empty;
+    for (int i = 1; i <= n; i++)
+    {
+        s += Math.Pow(i, p).ToString() + "\t ";
+    }
+
+    return s;
+}
+    // Ввод данных
+    int num = ReadData("Введите N: ");
+
+    // Собираем первую строчку таблицы
+    string line1 = LineBilder(num, 1);
+    // Собираем вторую строчку таблицы
+    string line2 = LineBilder(num, 3);
+
+    // Вывод данных
+    PrintData(line1, line2);
