d8c4a01dbe2db13224f8f99e111fbf3b5c236bd9
zalimus <zalim011@gmail.com>
2023-02-14 13:57:07 +0300

    add solution for task 41 43

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+// (от Голикова А.С.) * Пользователь вводит число нажатий, затем программа
+// следит за нажатиями с клавиатуры и выдает сколько чисел больше 0 было введено.
+
+
+
+Console.WriteLine("Введите числа через пробел: ");
+string input = Console.ReadLine() ?? "0";
+string[] numbers = input.Split(' ');
+
+int CountPositiveNumbers(string[] numbers)
+{
+    int count = 0;
+    foreach (string num in numbers)
+    {
+        if (int.Parse(num) > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count = CountPositiveNumbers(numbers);
+Console.WriteLine("Количество положительных чисел: " + count);
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// (от Голикова А.С.) * Найдите площадь треугольника образованного пересечением 3 прямых
+
+double ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return double.Parse(Console.ReadLine() ?? "0");
+}
+
+double b1 = ReadData("Введите число b1: ");
+double b2 = ReadData("Введите число b2: ");
+double k1 = ReadData("Введите число k1: ");
+double k2 = ReadData("Введите число k2: ");
+
+
+void IntersectionX(double b1, double b2, double k1, double k2)
+{
+    double x = (b2-b1)/(k1-k2);
+    Console.Write(x + "; ");
+}
+
+void IntersectionY(double b1, double b2, double k1, double k2)
+{
+    double y = k1*(b2-b1)/(k1-k2)+b1;
+    Console.WriteLine(y);
+}
+
+IntersectionX(b1, b2, k1, k2);
+IntersectionY(b1, b2, k1, k2);
